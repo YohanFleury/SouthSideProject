54fc2f0156bec5a70006e326c5c311f713c8864a
Yohan Fleury <yfleury95@gmail.com>
2023-03-04 09:49:45 +0100

    fitur

--- app/navigation/HomeNavigator/HomeNavigator.tsx
+++ app/navigation/HomeNavigator/HomeNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MaterialIcons, MaterialCommunityIcons, FontAwesome, Foundation } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
 
 import routes from '../routes';
 import HomeScreen from '../../screens/HomeScreen/HomeScreen';
@@ -9,24 +10,35 @@ import colors from '../../config/colors';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import NewPostScreen from '../../screens/NewPostScreen/NewPostScreen';
+import ChatScreen from '../../screens/ChatScreen/ChatScreen';
 
 export type HomeNavigatorParams = {
     Home: undefined;
     NewPost: undefined;
+    Chat: undefined;
+    Drawer: undefined;
     }
 const Stack = createNativeStackNavigator<HomeNavigatorParams>()
 
 const HomeNavigator = ({navigation}: any) => {
 
     return (
-    <Stack.Navigator>
-        <Stack.Screen name={routes.HOME} component={HomeScreen} options={
-         {
-         headerShown: false, fullScreenGestureEnabled: true, headerStyle: {backgroundColor: colors.dark.background}}
-         
-        } 
+    <Stack.Navigator screenOptions={{
+      }}>
+        <Stack.Screen name={routes.HOME} component={HomeScreen} options={{
+         headerShown: false, 
+         fullScreenGestureEnabled: true,
+         headerTransparent: true,
+         headerBlurEffect: 'dark',
+         headerTintColor: 'transparent',
+         gestureDirection: 'horizontal',
+        }} 
         
         />
+        <Stack.Screen name={routes.CHAT} component={ChatScreen} options={{
+            fullScreenGestureEnabled: true,
+            animation: 'slide_from_right',
+        }} />
     </Stack.Navigator>
    )
 }
--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, PanResponder, Dimensions, FlatList } from 'react-native';
-import CustomScreen from '../../components/CustomScreen/CustomScreen';
+import { View, StyleSheet, Dimensions, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { useAppSelector } from '../../redux/store';
+import { useNavigation, useScrollToTop } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { BottomSheetModal} from '@gorhom/bottom-sheet';
+import { BlurView } from 'expo-blur';
+
 import PostCard from '../../components/PostCard/PostCard';
-import { useScrollToTop } from '@react-navigation/native';
-import BottomSheet, {BottomSheetModalProvider, BottomSheetModal} from '@gorhom/bottom-sheet';
 import TipsModal from '../../components/Modals/TipsModal/TipsModal';
+import colors from '../../config/colors';
+import CustomText from '../../components/CustomText/CustomText';
+import { HomeNavigatorParams } from '../../navigation/HomeNavigator/HomeNavigator';
+import routes from '../../navigation/routes';
+
+const screenHeight =  Dimensions.get('window').height
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation<any>()
   const [postsList, setPostsList] = useState<any>()
   const reduxPostList = useAppSelector(state => state.users.postsList)
   const ref = React.useRef(null)
@@ -22,30 +33,45 @@ const HomeScreen = () => {
  
 
   return (
-      <CustomScreen>
-        <FlatList
-          data={postsList}
-          ref={ref}
-          renderItem={({ item }) => (
-            <PostCard
-            username={item.username}
-            name={item.name}
-            likes={item.likes}
-            description={item.description}
-            images={item.images}
-            onTipsPress={() => bottomSheetModalRef.current?.present()}
-            />
-          )}
-        />        
-        <TipsModal tipsModalRef={bottomSheetModalRef} />
-
-      </CustomScreen>
+      <View style={{flex: 1}}>
+        <View style={styles.mainContainer}>
+          
+          <FlatList
+            data={postsList}
+            ref={ref}
+            renderItem={({ item, index }) => (
+              <>
+                {index == 0 && <View style={{height: 0.1*screenHeight}} />}
+                <PostCard
+                source={'https://randomuser.me/api/portraits/men/2.jpg'}
+                username={item.username}
+                name={item.name}
+                likes={item.likes}
+                description={item.description}
+                images={item.images}
+                onTipsPress={() => bottomSheetModalRef.current?.present()}
+                />
+              </>
+            )}
+          />        
+            <BlurView tint='dark' intensity={100} style={[styles.header, StyleSheet.absoluteFill]}>
+              <Ionicons name="menu" size={31} color="white" onPress={() => navigation.openDrawer()} />
+              <CustomText style={{marginBottom: 5}}>Accueil</CustomText>
+              <Ionicons name="chatbox-outline" size={26} color="white" onPress={() => navigation.navigate(routes.CHAT)} />
+            </BlurView>
+          <TipsModal tipsModalRef={bottomSheetModalRef} />
+        </View>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     opacity: 0.1
+  },
+  mainContainer: {
+    backgroundColor: colors.dark.background,
+    flex: 1,
   },
   blurView: {
     position: 'absolute',
@@ -55,6 +81,14 @@ const styles = StyleSheet.create({
     right: 0,
     borderRadius: 15,
     overflow: "hidden"
+   },
+   header: {
+    height: 0.11*screenHeight,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingBottom: 10,
+    paddingHorizontal: 10, 
    },
 });
 
